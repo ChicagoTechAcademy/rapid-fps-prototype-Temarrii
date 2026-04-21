ceb7ebcb6d8ae74c108ad5f6d317df64e216d1ae
Temarrii <tmharrington@chicagotechacademy.org>
2026-04-21 11:46:25 -0500

    Video 2 (3:00)
    Finished video 1.

--- Source/RapidFPS/PlayerCharacter.cpp
+++ Source/RapidFPS/PlayerCharacter.cpp
@@ -47,7 +47,12 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 void APlayerCharacter::Move(const FInputActionValue& Value)
 {
-	UE_LOG(LogTemp, Warning, TEXT("You pressed a move button"));
+	FVector2D MovementVector = Value.Get <FVector2D>();
+	if (Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
 }
 
 void APlayerCharacter::Jump(const FInputActionValue& Value)
@@ -57,7 +62,12 @@ void APlayerCharacter::Jump(const FInputActionValue& Value)
 
 void APlayerCharacter::Look(const FInputActionValue& Value)
 {
-	UE_LOG(LogTemp, Warning, TEXT("You moved the mouse for looking"));
+	FVector2D LookAxisVector = Value.Get <FVector2D>();
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
 
 void APlayerCharacter::Shoot(const FInputActionValue& Value)
