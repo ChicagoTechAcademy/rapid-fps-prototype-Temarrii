1214b0f8419059070161d1488ac259421f1bc9cb
Temarrii <tmharrington@chicagotechacademy.org>
2026-04-22 11:20:14 -0500

    Video 2 (13:20)

--- Source/RapidFPS/FPSProjectile.h
+++ Source/RapidFPS/FPSProjectile.h
@@ -29,4 +29,11 @@ public:
 	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
 	USphereComponent* CollisionComponent;
 
+	// Projectile movement component.
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UProjectileMovementComponent* ProjectileMovementComponent;
+
+	// Function that initializes the projectile's velocity in the shoot direction.
+	void FireInDirection(const FVector& ShootDirection);
+
 };
